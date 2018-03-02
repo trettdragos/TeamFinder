1006b0e04db053a9df79629586fd1d651a533dc1
Unknown <trettdragos@yahoo.com>
2018-03-02 19:34:18 +0200

    Finished Login and Register

--- server.js
+++ server.js
@@ -3,8 +3,9 @@ var app = express();
 var sleep = require('sleep');
 var path = require('path');
 var mysql = require('mysql');
-var passport = require('passport');
-var Strategy = require('passport-local').Strategy;
+let cookieParser = require('cookie-parser');
+let bodyParser = require('body-parser');
+let xss = require('xss');
 var server = require('http').createServer(app);
 var io = require('socket.io').listen(server, {
     log: false,
@@ -12,88 +13,90 @@ var io = require('socket.io').listen(server, {
     origins: '*:*'
 });
 
-var user=[{
-  ID:"",
-  EMAIL:"",
-  USERNAME:"",
-  PASSWORD:""
-}]
-
 var con = mysql.createConnection({
-  host: "localhost",x 
+  host: "localhost",
   user: "root",
   password: "password",
   database: "TeamFinder"
 });
 
-passport.use(new Strategy(
-  function(username, password, cb) {
-    con.query("SELECT * FROM accounts WHERE EMAIL = ? AND PASSWORD = ? LIMIT 1", [username, password], function (err, result, fields) {
-    if (err) throw err;
-    if(result.EMAIL = username){
-      user = result;
-      return cb(null, result);
-    }
-    else return cb(null, false);
-  });
-  }
-));
-
-passport.serializeUser(function(user, cb) {
-  cb(null, user);
-});
-
-passport.deserializeUser(function(user, cb) {
-  cb(null, user);
-});
-
-app.use(require('morgan')('combined'));
-app.use(require('cookie-parser')());
-app.use(require('body-parser').urlencoded({ extended: true }));
-app.use(require('express-session')({ secret: 'keyboard cat', resave: false, saveUninitialized: false }));
-app.use(passport.initialize());
-app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(cookieParser());
 
 io.on('connection', function(socket) {
-    console.log('Client connected...');
-    socket.on('reply with event', function(data){
-      //listen for changes in the db
-      con.query("SELECT * FROM accounts WHERE EMAIL = ? AND PASSWORD = ? LIMIT 1", [user[0].EMAIL, user[0].PASSWORD], function (err, result, fields) {
-        if (err) throw err;
-        user=result;
-      });
-      socket.emit('received event', data);
+    console.log('Client connected...'+socket.id);
+
+    socket.on('auth login',function (user) {
+        con.query("SELECT * FROM accounts WHERE EMAIL = ? AND PASSWORD = ? LIMIT 1", [user.email, user.password], function (err, result, fields) {
+          if (err) throw err;
+          if(result[0]){
+            console.log("auth succesfull with user: "+user.email);
+            socket.emit('auth login', {status:"succesfull", email:user.email});
+          }
+          else{
+            console.log("auth failed for user: "+user.email);
+            socket.emit('auth login', {status:"failed", email:user.email});
+          }
+        });
     });
+
+    socket.on('auth register', function(user){
+      console.log("checking if user: "+user.email+" is already in the db...")
+      con.query("SELECT * FROM accounts WHERE EMAIL = ? LIMIT 1", [user.email], function (err, result, fields) {
+          if (err) throw err;
+          if(result[0]){
+            console.log("user with email "+user.email+" already exists");
+            socket.emit('auth register', {status:"Email already used.", email:user.email});
+          }
+          else{
+            console.log("registering user: "+user.name+" with the email: "+user.email);
+            con.query("INSERT INTO accounts (ID, USERNAME, EMAIL, PASSWORD, LINKEDIN, GITHUB, SKILLS) VALUES (?, ?, ?, ?, ?, ?, ?)", [0, user.name, user.email, user.password, user.linkedin, user.github, JSON.stringify(user.skills)], function (err, result) {
+              if (err) throw err;
+              socket.emit('auth register', {status:"succesfull", email:user.email});
+            });
+          }
+        });
+    });
+
     socket.on('disconnect', function () {
       console.log("Client disconected...");
   });
 });
-
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.get('/', function(req, res) {
-  if(!req.user){
+  if(req.cookies.username){
+    res.render('pages/index.ejs', {email:req.cookies.username});
+  }
+  else{
     res.redirect('/login');
   }
-  res.render('pages/index', { user: req.user })
+});
+
+app.get('/dashboard', function(req, res) {
+  if(req.cookies.username){
+    res.render('pages/index.ejs', {email:req.cookies.username});
+  }
+  else{
+    res.redirect('/login');
+  }
 });
 
 app.get('/login', function(req, res){
   res.render('pages/login');
 });
 
-app.post('/login', 
-  passport.authenticate('local', { failureRedirect: '/login' }),
-  function(req, res) {
-    res.redirect('/');
-  });
+app.get('/logout', function(req, res){
+    res.render('pages/logout');
 
-app.get('/logout',
-  function(req, res){
-    req.logout();
-    res.redirect('/');
-  });
+});
+
+app.get('/register', function(req, res){
+  res.render('pages/register');
+});
 
 server.listen(3000);
 
